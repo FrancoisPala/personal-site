e7f84127f255064ac0ab737a3c0311001a9c4e2a
François Palà <fpala@kilix.fr>
2017-08-08 10:58:18 +0200

    cleaner sass

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,9 +13,52 @@ const STYLES = [
 ];
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            width: window.innerWidth,
+        };
+    }
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+    // version mobile: chaque map en dessous de l'autre
+    // changer la background color et l'agencement
+    // changer le menu
     render() {
         const style = this.props.match.params.style ? this.props.match.params.style : 'outrun';
+        const { width } = this.state;
+        const isMobile = width <= 500;
+        console.log(width, isMobile);
         if (!R.contains(style, STYLES)) return <Redirect to="/outrun" />;
+        if (isMobile) {
+            return (
+            <div id={`Presentation-${style}`}>
+                <Menu />
+                <img id='nameLogo' src={require(`../../assets/${style}_name.svg`)} alt="My Name"/>
+                <h3>BACK-END DEVELOPER</h3>
+                {/*<Sidepic side='left' style={style}/>*/}
+                <p> Do you like Music? Video Games? Travelling? <a href='https://www.linkedin.com/in/francoispala/' target='blank' id='letsChat'>Let's chat!</a><br />
+                    <mark>I'm a node.js / react.js developer</mark>, soon-to-be based in New York City.<br />
+                    <mark>I'm open for work</mark>, whether you're a big company, a startup or just want my freelance services.<br />
+                    If you want to learn a little bit more about me, feel free to check both maps under this text :)<br /></p>
+                {/*<Sidepic side='right' style={style}/>*/}
+                <div className={`mapWrapper mobile`}>
+                    <Map id={`Map-leftBlock-${style}`} type='leftMap' mapStyle={style} position={[37.6, -95.665]} zoom={4}/>
+                    <Map id={`Map-rightBlock-${style}`} type='rightMap' mapStyle={style} position={[48.864716, 2.349014]} zoom={10}/>
+                </div>
+            </div>
+            );
+        }
         return (
             <div id={`Presentation-${style}`}>
                 <Menu />
@@ -27,7 +70,7 @@ class Home extends Component {
                     <mark>I'm open for work</mark>, whether you're a big company, a startup or just want my freelance services.<br />
                     If you want to learn a little bit more about me, feel free to check both maps under this text :)<br /></p>
                 {/*<Sidepic side='right' style={style}/>*/}
-                <div id='mapWrapper'>
+                <div className={`mapWrapper desktop`}>
                     <Map id={`Map-leftBlock-${style}`} type='leftMap' mapStyle={style} position={[37.6, -95.665]} zoom={4}/>
                     <Map id={`Map-rightBlock-${style}`} type='rightMap' mapStyle={style} position={[48.864716, 2.349014]} zoom={10}/>
                 </div>
